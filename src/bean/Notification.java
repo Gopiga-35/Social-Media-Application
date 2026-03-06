2ba029116fa78bf04110ee56d8f0abb9ad99a192
Gopiga-35 <gopigaramachandran35@gmail.com>
2026-03-06 14:08:39 +0530

    commit SocialMedia

---
+++ src/bean/Notification.java
@@ -0,0 +1,14 @@
+package bean;
+
+public class Notification {
+
+    private String message;
+
+    public Notification(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
